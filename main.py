e967d63a01ef89a6a37100473b0a6de929e2cd2f
nick <nick1998chen0725@gmail.com>
2022-03-26 20:45:45 +0800

    make it better

--- main.py
+++ main.py
@@ -22,20 +22,16 @@ setting = Setting()
 
 app = Flask(__name__)
 
+# 資料庫設定
 db_url = setting.database_url
 if db_url.startswith("postgres://"):
     db_url = db_url.replace("postgres://", "postgresql://", 1)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db.init_app(app)
 
 line_bot_api = LineBotApi(setting.channel_token)
 handler = WebhookHandler(setting.channel_secret)
-
-# 資料庫設定
-db.init_app(app)
-
-use_model = [Cycle, Cotton, PredictDate, Name]
 
 
 # 主路由
